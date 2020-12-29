6ff105783c371fb6a9f5162ab8585b0c0ad5d407
Robin Scheibler <fakufaku@gmail.com>
2020-12-30 02:37:28 +0900

    Atomic update of GPS data fields including both GGA and RMC sentences. Display stuff moved to a separate class.

--- bgeigie_firmware_pio/src/display.cpp
+++ bgeigie_firmware_pio/src/display.cpp
@@ -1,6 +1,71 @@
 #include <M5Stack.h>
 #include <display.hpp>
 
+void Display::setup() {
+  // Clear display
+  M5.Lcd.clear(); 
+  M5.lcd.setRotation(3);
+
+  //Display safecast copyright
+  M5.Lcd.setTextColor(TFT_WHITE,TFT_BLACK);
+  M5.Lcd.drawString("SAFECAST", 230, 215, 1);
+  M5.Lcd.setTextColor(TFT_ORANGE,TFT_BLACK);
+  M5.Lcd.drawString("2020", 285, 215, 1);
+
+  // mark as ready
+  _ready = true;
+}
+
+void Display::update(const GeigerMeasurement &geiger_count) {
+  if (!_ready)
+    return;
+
+  M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+
+  //Display CPM  
+  M5.Lcd.setCursor(20,50);
+  M5.Lcd.drawString("CPM  =",5, 50, 4);
+  M5.Lcd.setCursor(120,55);
+  printIntFont(geiger_count.per_minute(), true, 5, 50, 90, 4);
+
+  //Display uSv/h
+  M5.Lcd.setCursor(22,70);
+  M5.Lcd.drawString("uSv/h =",5, 70, 4);
+  M5.Lcd.setCursor(100,80);
+  printFloatFont(geiger_count.uSv(), true, 7, 3, 70, 90, 4);
+}
+
+void Display::update (GPSSensor &gps) {
+  // Here we can't make the ref const because we need to access the fields
+  // of the TinyGPS object directly...
+
+  M5.Lcd.setCursor(0 , 150);
+  M5.Lcd.setTextColor(WHITE, BLACK);
+  M5.Lcd.print("Satelites  :");
+  printInt(gps.data().satellites.value(), gps.data().satellites.isValid(), 5);
+  M5.Lcd.println();
+  M5.Lcd.print("Latitude   :");
+  printFloat(gps.data().location.lat(), gps.data().location.isValid(), 11, 6);
+  M5.Lcd.println();
+  M5.Lcd.print("Longitude  :");
+  printFloat(gps.data().location.lng(), gps.data().location.isValid(), 12, 6);
+  M5.Lcd.println();
+  M5.Lcd.print("Date       :");
+  printDate(gps.data().date);
+  M5.Lcd.println();
+  M5.Lcd.print("Time       :");
+  printTime(gps.data().time);
+  M5.Lcd.println();
+  M5.Lcd.print("Altitude   :");
+  printFloat(gps.data().altitude.meters(), gps.data().altitude.isValid(), 7, 2);
+  M5.Lcd.println();
+  M5.Lcd.print("Degree     :");
+  printFloat(gps.data().course.deg(), gps.data().course.isValid(), 7, 2);
+  M5.Lcd.println();
+  M5.Lcd.print("Speed      :");
+  printFloat(gps.data().speed.kmph(), gps.data().speed.isValid(), 6, 2);
+}
+
 void printFloat(float val, bool valid, int len, int prec)
 {
   if (!valid)
@@ -80,7 +145,6 @@ void printIntFont(unsigned long val, bool valid, int len, int y, int x, int font
   delay(0);
 }
 
-/*
 //Prints date
 void printDate(TinyGPSDate &d)
 {
@@ -114,4 +178,3 @@ void printTime(TinyGPSTime &t)
   }
   delay(0);
 }
-*/
--- bgeigie_firmware_pio/src/main.cpp
+++ bgeigie_firmware_pio/src/main.cpp
@@ -2,13 +2,18 @@
 
 #include <M5Stack.h>
 #include <hardwarecounter.hpp>
+#include <gps.hpp>
 #include <display.hpp>
+#include <config.hpp>
 
-// Peripherals
-HardwareCounter pulse_counter(5000, 2);  // pulse counter with 12 bin moving average
+// Peripherals setup
+// - pulse counter with 12 bin moving average
+HardwareCounter pulse_counter(GEIGER_AVERAGING_PERIOD_MS, GEIGER_PULSE_GPIO);
+GeigerMeasurement geiger_count(GEIGER_SENSOR1_CPM_FACTOR);
+// - GPS sensor
+GPSSensor gps(GPS_SERIAL_NUM, GPS_BAUD_RATE);
 
-const float sensor1_cpm_factor = 340;
-GeigerMeasurement<12> geiger_count(sensor1_cpm_factor);
+Display display(LCD_REFRESH_RATE);
 
 void setup()
 {
@@ -17,67 +22,30 @@ void setup()
   Serial.begin(115200);
   Serial.println("test");
 
-  // Clear display
-  M5.Lcd.clear(); 
-  M5.lcd.setRotation(3);
-
-  //Beeps at startup
-  // M5.Speaker.tone(900, 1000);
-  // delay(10);
-  // M5.Speaker.mute();
-
-  //Reset text padding to zero (default)
-  // M5.Lcd.setTextPadding(0);
-  // M5.Lcd.drawString("bGeigieRaku V0.5",20, 100, 4);
-
-  //Display safecast copyright
-  M5.Lcd.setTextColor(TFT_WHITE,TFT_BLACK);
-  M5.Lcd.drawString("SAFECAST", 230, 215, 1);
-  M5.Lcd.setTextColor(TFT_ORANGE,TFT_BLACK);
-  M5.Lcd.drawString("2020", 285, 215, 1);
+  display.setup();
 
   // Reset the pulse counter before starting
   pulse_counter.reset();
 }
 
-//------------------------------------------------------------------------------------------------------------------------------------------
-
 void loop()
 {
 
+  // Run all the non-blocking update routines here
+  gps.update();
   pulse_counter.update();
 
-  // Send reading data if IS_READY is passed when 
-  if (pulse_counter.available()) {
+  if (gps.available()) {
+    // do the stuff we want to do upon GPS update here
+    display.update(gps);
+  }
 
+  // Process the new geiger count when available
+  if (pulse_counter.available()) {
     // update the Geiger counter with the new measurement
     geiger_count.feed(pulse_counter.get_last_count());
-
-    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-    //Display CPM  
-    M5.Lcd.setCursor(20,50);
-    M5.Lcd.drawString("CPM  =",5, 50, 4);
-    M5.Lcd.setCursor(120,55);
-    // printInt(cpm, true, 5);
-    printIntFont(geiger_count.per_minute(), true, 5, 50, 90, 4);
-
-    //Display uSv/h
-    M5.Lcd.setCursor(22,70);
-    M5.Lcd.drawString("uSv/h =",5, 70, 4);
-    M5.Lcd.setCursor(100,80);
-    printFloatFont(geiger_count.uSv(), true, 7, 3, 70, 90, 4);
-
-    //prints data
-    Serial.print("cpb (60 bins) =");
-    Serial.println(geiger_count.per_bin());
-    Serial.print("cpm =");
-    Serial.println(geiger_count.per_minute());
-    Serial.print("uSvH =");
-    Serial.println(geiger_count.uSv());    
-    Serial.print("S1peak =");
-    Serial.println(geiger_count.peak_per_minute()); 
-    Serial.print("Total counts =");
-    Serial.println(geiger_count.total());
-    Serial.println();
+    // immediately update the display
+    display.update(geiger_count);
   }
+
 }
