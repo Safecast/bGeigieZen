552bd749c132c80159f84206d892fe02efd150a6
Louis Bertrand <louis@bertrandtech.ca>
2023-04-14 08:39:32 -0400

    Start on WiFi config menu

--- bgeigie_firmware_pio/src/menu.cpp
+++ bgeigie_firmware_pio/src/menu.cpp
@@ -4,6 +4,9 @@
 #include <display.hpp>
 #include <menu.hpp>
 
+// InitState mstate{};
+ConfigState cfgstate{};
+
 DimBlankTiming::DimBlankTiming(uint32_t delay_dimming,
                                 uint32_t delay_blanking,
                                 const char *item_label) :
@@ -59,6 +62,8 @@ void InitState::on_entry(MenuContext *context) {
   M5.Lcd.drawString("Menu InitState",10, 20, 2);
 
   Button &dbbutton = context->button_dimblank;
+  Button &cfgbutton = context->button_config_network;
+  
   // get the current values of dim/blank timing
   auto dimblankchoice = context->dimblank_choices[context->dimblank_idx];
   context->button_dimblank.draw();
@@ -68,6 +73,13 @@ void InitState::on_entry(MenuContext *context) {
   M5.Lcd.drawString(dimblankchoice.label,
                     dbbutton.x+(dbbutton.w/2),
                     dbbutton.y+(dbbutton.h/2), 4);
+  context->button_config_network.draw();
+  M5.Lcd.setTextDatum(ML_DATUM);
+  M5.Lcd.drawString("WiFi Config", 10, cfgbutton.y+(cfgbutton.h/2), 4);
+  M5.Lcd.setTextDatum(MC_DATUM);
+  M5.Lcd.drawString("START",
+                    cfgbutton.x+(cfgbutton.w/2),
+                    cfgbutton.y+(cfgbutton.h/2), 4);
 }
 
 void InitState::on_exit(MenuContext *context) {/*DEBUG*/ Serial.println("InitState::on_exit");}
@@ -77,7 +89,12 @@ void InitState::update(MenuContext *context) {
 
   // get the current values of dim/blank timing
   Button &dbbutton = context->button_dimblank;
-  if(dbbutton.wasReleased()) {
+  Button &cfgbutton = context->button_config_network;
+if(cfgbutton.wasReleased()) {
+  /*DEBUG*/ Serial.println("Config Button was pressed/released");
+  context->goto_state(&cfgstate);
+}
+else if(dbbutton.wasReleased()) {
     // cycle through all choices
     context->dimblank_idx = context->dimblank_idx >= 4 ?
                             0 : context->dimblank_idx + 1;
@@ -95,4 +112,39 @@ void InitState::update(MenuContext *context) {
                       dbbutton.x+(dbbutton.w/2),
                       dbbutton.y+(dbbutton.h/2), 4);
   }
+}
+
+/* 
+ * Configure the IP networking with captive access point
+*/
+void ConfigState::on_entry(MenuContext *context) {
+  /*DEBUG*/ Serial.println("ConfigState::on_entry");
+  M5.Lcd.clear();
+  M5.lcd.setRotation(3);
+  M5.Lcd.setTextDatum(BL_DATUM);  // By default, text x,y is bottom left corner
+
+  /** TODO: Eliminate duplication from Display::draw_base and draw_navbar */
+  // Display safecast copyright
+  M5.Lcd.setTextFont(1);
+  M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+  M5.Lcd.drawString("SAFECAST", 230, 215, 1);
+  M5.Lcd.setTextColor(TFT_ORANGE, TFT_BLACK);
+  M5.Lcd.drawString("2023", 285, 215, 1);
+
+  // Navbar: show only one button (N.B. hot zones are still live)
+  M5.Lcd.setTextColor(TFT_YELLOW, TFT_BLACK);
+  auto previous_datum = M5.Lcd.getTextDatum();
+  M5.Lcd.setTextDatum(BC_DATUM);
+  Button &b = M5.BtnB;
+  M5.Lcd.drawString("RETURN", b.x+(b.w/2), b.y+(b.h-10), 2);
+  M5.Lcd.setTextDatum(previous_datum);
+
+  // Draw the initial menu and set up the buttons.
+  M5.Lcd.drawString("Menu ConfigState",10, 20, 2);
+
+}
+void ConfigState::on_exit(MenuContext *context) {/*DEBUG*/ Serial.println("ConfigState::on_exit");}
+
+void ConfigState::update(MenuContext *context) {
+  /*DEBUG*/ //Serial.println("ConfigState::update");
 }
