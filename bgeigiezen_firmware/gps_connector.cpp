808f2202f74066a35c07fcad2949a860781e34d1
Louis Bertrand <louis@bertrandtech.ca>
2023-07-09 13:38:36 -0400

    GPS connector works, remove debug printf lines.

--- bgeigiezen_firmware/gps_connector.cpp
+++ bgeigiezen_firmware/gps_connector.cpp
@@ -12,25 +12,20 @@ bool GpsConnector::activate(bool retry) {
 int8_t GpsConnector::produce_data() {
   // Poll the serial port and feed the encoder
   while (ss.available() > 0) {
-    uint8_t c = 0;
-    c = ss.read();
-    Serial.printf("%c", c);
-    gps.encode(c);
+    gps.encode(ss.read());
   }
-  // Serial.printf("GPS chars processed: %d\n", gps.charsProcessed());
 
   if (gps.location.isValid()) {
-    Serial.println("gps.location.isValid returned true.");
     data.available = true;
-    data.updated = gps.location.isUpdated();// but not necessarily changed.
+    data.updated = gps.location.isUpdated(); // but not necessarily changed.
     data.age = gps.location.age();
     data.longitude = gps.location.lng();
     data.latitude = gps.location.lat();
     data.altitude = gps.altitude.meters();
     data.satellites = gps.satellites.value();
     return e_worker_data_read;
-  } else {
-    // Serial.println("gps.location.isValid returned false.");
+  }
+  else {
     data.available = false;
     return e_worker_idle;
   }
