44c9e62058401c3f1c5861f884d4b2955dff5c60
Louis Bertrand <louis@bertrandtech.ca>
2023-07-09 10:47:10 -0400

    Debug printf to show GPS records as they come in.

--- bgeigiezen_firmware/gps_connector.cpp
+++ bgeigiezen_firmware/gps_connector.cpp
@@ -12,10 +12,15 @@ bool GpsConnector::activate(bool retry) {
 int8_t GpsConnector::produce_data() {
   // Poll the serial port and feed the encoder
   while (ss.available() > 0) {
-    gps.encode(ss.read());
+    uint8_t c = 0;
+    c = ss.read();
+    Serial.printf("%c", c);
+    gps.encode(c);
   }
+  // Serial.printf("GPS chars processed: %d\n", gps.charsProcessed());
 
   if (gps.location.isValid()) {
+    Serial.println("gps.location.isValid returned true.");
     data.available = true;
     data.updated = gps.location.isUpdated();// but not necessarily changed.
     data.age = gps.location.age();
@@ -25,6 +30,7 @@ int8_t GpsConnector::produce_data() {
     data.satellites = gps.satellites.value();
     return e_worker_data_read;
   } else {
+    // Serial.println("gps.location.isValid returned false.");
     data.available = false;
     return e_worker_idle;
   }
