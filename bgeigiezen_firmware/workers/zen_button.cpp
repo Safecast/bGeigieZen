6614d36f59eeb22cf7907653b2b21936435a67cd
robouden <rob@safecast.org>
2024-01-24 19:48:48 +0900

    Fix for grey menu switching by itself.

--- bgeigiezen_firmware/workers/zen_button.cpp
+++ bgeigiezen_firmware/workers/zen_button.cpp
@@ -1,13 +1,15 @@
 #include "zen_button.h"
 #include "debugger.h"
 
+#ifdef M5_GREY
+#define SHORT_PRESS_DURATION 100
+#else
 #define SHORT_PRESS_DURATION 10
+#endif
 #define LONG_PRESS_DURATION 2400
 
-
-ZenButton::ZenButton(Button& m5_button) :
-    Worker<ButtonState>({false, false, false}),
-    _m5_button(m5_button) {
+ZenButton::ZenButton(Button& m5_button) : Worker<ButtonState>({false, false, false}),
+                                          _m5_button(m5_button) {
 }
 
 ZenButton::~ZenButton() {
@@ -18,11 +20,12 @@ bool ZenButton::activate(bool retry) {
 }
 
 int8_t ZenButton::produce_data() {
+  data.longPress = data.currentlyPressed && _m5_button.wasReleasefor(LONG_PRESS_DURATION);
+  data.shortPress = data.currentlyPressed && _m5_button.wasReleasefor(SHORT_PRESS_DURATION) && !data.longPress;
   data.currentlyPressed = _m5_button.isPressed();
-  data.longPress = _m5_button.wasReleasefor(LONG_PRESS_DURATION);
-  data.shortPress = _m5_button.wasReleasefor(SHORT_PRESS_DURATION) && !data.longPress;
+
   if (data.longPress || data.shortPress) {
-//    DEBUG_PRINTLN("Button pressed");
+    DEBUG_PRINTLN("Button pressed");
     return e_worker_data_read;
   }
   return e_worker_idle;
