3c6b293a6e719f7e2ee27ed4f29fe912e77e71f0
robouden <rob@safecast.org>
2024-12-11 09:17:45 +0900

    Added the explanation of the code (Codeium did it)

--- bgeigiezen_firmware/handlers/debug_logger.cpp
+++ bgeigiezen_firmware/handlers/debug_logger.cpp
@@ -12,7 +12,6 @@
 
 /// BaseDebugLogger
 
-/*************  ✨ Codeium Command ⭐  *************/
 /**
  * @brief Constructor for BaseDebugLogger
  *
@@ -22,12 +21,10 @@
  * Note that this constructor is protected, meaning that it can only be called
  * from the constructors of derived classes.
  */
-/******  e0f75267-1cb5-4c2c-a952-0f649ea223b8  *******/
 BaseDebugLogger::BaseDebugLogger(LocalStorage& config, const char* logging_name) : Handler(), _config(config), _logging_name(""), _logging_to(""), _is_temp(true), _total(0) {
   strcpy(_logging_name, logging_name);
 }
 
-/*************  ✨ Codeium Command ⭐  *************/
 /**
  * Activate the debug logger. If the SD card is not ready for writing, then
  * immediately return false. Otherwise, create a new temporary log file
@@ -39,7 +36,6 @@ BaseDebugLogger::BaseDebugLogger(LocalStorage& config, const char* logging_name)
  *
  * @return true if the logger was activated, false otherwise.
  */
-/******  8e45a78d-1bf4-4b27-b39a-9478339104b8  *******/
 bool BaseDebugLogger::activate(bool) {
   // Check SD readiness
   if (!SDInterface::i().can_write_logs()) {
@@ -56,13 +52,11 @@ bool BaseDebugLogger::activate(bool) {
   return true;
 }
 
-/*************  ✨ Codeium Command ⭐  *************/
   /**
    * Deactivate the debug logger. If the number of lines logged is less than
    * MIN_LOG_LINES_FOR_KEEP, then delete the log file. Otherwise, log the
    * total number of lines logged. Clear the log file name.
    */
-/******  41f3248b-fdec-4bcb-a760-72e16da28360  *******/
 void BaseDebugLogger::deactivate() {
   if (_total < MIN_LOG_LINES_FOR_KEEP) {
     // Delete
@@ -74,7 +68,6 @@ void BaseDebugLogger::deactivate() {
   strcpy(_logging_to, "");
 }
 
-/*************  ✨ Codeium Command ⭐  *************/
   /**
    * Handle produced work. If the log aggregator has new data, and the sd card is
    * ready, then write the line to the log file. If the log file is currently
@@ -85,7 +78,6 @@ void BaseDebugLogger::deactivate() {
    * @param workers
    * @return
    */
-/******  67e2355c-5e52-4edc-9f0e-16f42b029155  *******/
 int8_t BaseDebugLogger::handle_produced_work(const worker_map_t& workers) {
   const auto& log_data = workers.worker<LogAggregator>(k_worker_log_aggregator);
   if (log_data->is_fresh()) {
@@ -120,7 +112,6 @@ int8_t BaseDebugLogger::handle_produced_work(const worker_map_t& workers) {
 
 /// GpsDebugLogger
 
-/*************  ✨ Codeium Command ⭐  *************/
 /**
  * @brief Write a line to the debug log using the latest GPS and NavSat data
  *
@@ -128,7 +119,6 @@ int8_t BaseDebugLogger::handle_produced_work(const worker_map_t& workers) {
  *
  * @return true if the line was written, false otherwise
  */
-/******  387bb3b6-2151-4835-9dfa-79a5e965ae24  *******/
 bool GpsDebugLogger::write_line(const worker_map_t& workers) {
   const auto& gps = workers.worker<GpsConnector>(k_worker_navsat_collector);
   const auto& navsat = workers.worker<NavsatCollector>(k_worker_navsat_collector);
