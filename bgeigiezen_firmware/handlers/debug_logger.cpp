d95adb468849c39b15cc5e904457236aba706f67
robouden <rob@safecast.org>
2025-03-25 10:36:41 +0900

    GPS logger  timestamp fix

--- bgeigiezen_firmware/handlers/debug_logger.cpp
+++ bgeigiezen_firmware/handlers/debug_logger.cpp
@@ -4,12 +4,10 @@
 #include "workers/navsat_collector.h"
 #include "workers/rtc_connector.h"
 
-
 // e.g. /debug/gps-latest.log
 #define TEMP_LOG_NAME_F "%s/%s-latest.log"
 // e.g. /debug/gps-2023-11-20_1230.log
 #define DATED_LOG_NAME_F "%s/%s-%04d-%02d-%02d_%02d%02d.log"
-
 
 /// BaseDebugLogger
 
@@ -63,7 +61,7 @@ bool BaseDebugLogger::activate(bool) {
   return true;
 }
 
-  /**
+/**
    * Deactivate the debug logger. If the number of lines logged is less than
    * MIN_LOG_LINES_FOR_KEEP, then delete the log file. Otherwise, log the
    * total number of lines logged. Clear the log file name.
@@ -73,13 +71,14 @@ void BaseDebugLogger::deactivate() {
     // Delete
     M5_LOGD("End logging to '%s', deleting file due to insufficient lines (%d).", _logging_to, _total);
     SDInterface::i().delete_log(_logging_to);
-  } else {
+  }
+  else {
     M5_LOGD("End logging to '%s', total of %d lines logged.", _logging_to, _total);
   }
   strcpy(_logging_to, "");
 }
 
-  /**
+/**
    * Handle produced work. If the log aggregator has new data, and the sd card is
    * ready, then write the line to the log file. If the log file is currently
    * temporary, then use the rtc to rename the log file. If the write succeeds,
@@ -120,9 +119,7 @@ int8_t BaseDebugLogger::handle_produced_work(const worker_map_t& workers) {
   return e_handler_idle;
 }
 
-
 /// GpsDebugLogger
-
 
 bool GpsDebugLogger::can_activate() {
   return gnss.getUbloxModuleType() != UBLOX_UNKNOWN_MODULE;
@@ -130,7 +127,7 @@ bool GpsDebugLogger::can_activate() {
 
 void GpsDebugLogger::write_header_lines() {
   // Write log format when creating a new log
-  SDInterface::i().log_println(_logging_to,  "# hAcc,vAcc,velN,velE,velD,gSpeed,headMot,sAcc,headAcc,invalidLlh");
+  SDInterface::i().log_println(_logging_to, "# hAcc,vAcc,velN,velE,velD,gSpeed,headMot,sAcc,headAcc,invalidLlh");
 }
 
 /**
@@ -144,15 +141,18 @@ bool GpsDebugLogger::write_line(const worker_map_t& workers) {
   const auto& gps = workers.worker<GpsConnector>(k_worker_navsat_collector);
   const auto& navsat = workers.worker<NavsatCollector>(k_worker_navsat_collector);
   const auto& rtc_data = workers.worker<DateTimeProvider>(k_worker_rtc_connector)->get_data();
-  
-  if (gps->active() && navsat->active()) {
-    char log_string[250];  // Increased buffer size to accommodate timestamp
-    char timestamp[21];    // ISO 8601 format: "YYYY-MM-DDTHH:MM:SSZ"
 
-    // // Retrieve and format the timestamp
-    time_t currentTime =rtc_data.year + rtc_data.month + rtc_data.day + rtc_data.hour + rtc_data.minute + rtc_data.second;
-    struct tm* utcTime = gmtime(&currentTime);
-    strftime(timestamp, sizeof(timestamp), "%Y-%m-%dT%H:%M:%SZ", utcTime);
+  if (gps->active() && navsat->active()) {
+    char log_string[250]; // Increased buffer size to accommodate timestamp
+    char timestamp[21];   // ISO 8601 format: "YYYY-MM-DDTHH:MM:SSZ"
+
+    sprintf(
+        timestamp,
+        "%04d-%02d-%02dT%02d:%02d:%02dZ",
+        rtc_data.year, rtc_data.month, rtc_data.day, rtc_data.hour, rtc_data.minute, rtc_data.second);
+
+    // M5_LOGD("Set GPS logger date to: %04d-%02d-%02d %02d:%02d:%02d",
+    //         rtc_data.year, rtc_data.month, rtc_data.day, rtc_data.hour, rtc_data.minute, rtc_data.second);
 
     snprintf(
         log_string, sizeof(log_string),
@@ -167,8 +167,7 @@ bool GpsDebugLogger::write_line(const worker_map_t& workers) {
         gps->get_data().headMot,
         gps->get_data().sAcc,
         gps->get_data().headAcc,
-        gps->get_data().invalidLlh ? '1' : '0'
-    );
+        gps->get_data().invalidLlh ? '1' : '0');
 
     return SDInterface::i().log_println(_logging_to, log_string);
   }
