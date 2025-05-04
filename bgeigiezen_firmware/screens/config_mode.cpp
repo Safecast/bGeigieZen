68e2d9923034c57e34bf1208582867a929352e24
robouden <rob@safecast.org>
2025-05-04 16:26:52 +0900

    Make SD Card Wipe warning box a proper square with consistent dimensions

--- bgeigiezen_firmware/screens/config_mode.cpp
+++ bgeigiezen_firmware/screens/config_mode.cpp
@@ -243,17 +243,17 @@ void ConfigModeScreen::render_sd_wipe(const worker_map_t& workers, const handler
   drawButton3("Menu");
 
   // Create warning box with red background and white text
-  M5.Lcd.fillRect(0, 70, 320, 40, LCD_COLOR_ERROR);
-  M5.Lcd.setCursor(10, 80, &fonts::Font2);
+  M5.Lcd.fillRect(0, 70, 320, 60, LCD_COLOR_ERROR);
+  M5.Lcd.setCursor(10, 85, &fonts::Font2);
   M5.Lcd.setTextColor(TFT_WHITE, LCD_COLOR_ERROR);
-  M5.Lcd.printf("WARNING: This will delete ALL log files\n");
-  M5.Lcd.setCursor(10, 100, &fonts::Font2);
+  M5.Lcd.printf("WARNING: This will delete ALL log files");
+  M5.Lcd.setCursor(10, 105, &fonts::Font2);
   M5.Lcd.printf("from the SD card!");
   
   // Regular instructions below the warning
-  M5.Lcd.setCursor(0, 120, &fonts::Font2);
+  M5.Lcd.setCursor(0, 140, &fonts::Font2);
   M5.Lcd.setTextColor(LCD_COLOR_DEFAULT, LCD_COLOR_BACKGROUND);
-  M5.Lcd.printf("\nPress WIPE to confirm deleting all log files.\n");
+  M5.Lcd.printf("Press WIPE to confirm deleting all log files.\n");
   M5.Lcd.printf("Your device settings will be preserved.\n");
 }
 
