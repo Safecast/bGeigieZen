d3b8c5b315907d88fbbdc22ca29573ef847e83da
robouden <rob@safecast.org>
2025-05-05 01:32:32 +0900

    Fix Survey Mode display: Use CP5S instead of CPS for more stable readings, respect user settings for main display value, fix continuous updates

--- bgeigiezen_firmware/screens/survey_mode.cpp
+++ bgeigiezen_firmware/screens/survey_mode.cpp
@@ -56,6 +56,7 @@ void SurveyModeScreen::render(const worker_map_t& workers, const handler_map_t& 
       }
     }
     first_render = false;
+    force = true; // Force a full render on first display
   }
 
   // Check if we're currently logging
@@ -82,56 +83,72 @@ void SurveyModeScreen::render(const worker_map_t& workers, const handler_map_t& 
   drawButton2("");
   drawButton3("Menu");
 
+  // Get references to sensors
+  const auto& gm_sensor = workers.worker<GeigerCounter>(k_worker_gm_sensor);
+  const auto& gps = workers.worker<GpsConnector>(k_worker_gps_connector);
+
+  // Clear screen content on force render
   if (force) {
     clear_screen_content();
+  }
 
-    // Display radiation data from the GM sensor
-    const auto& gm_sensor = workers.worker<GeigerCounter>(k_worker_gm_sensor);
-    const auto& gps = workers.worker<GpsConnector>(k_worker_gps_connector);
+  // Always update the radiation data when fresh
+  if (gm_sensor->is_fresh() || force) {
+    // Display values based on user settings
+    const auto& settings = workers.worker<LocalStorage>(k_worker_local_storage);
+    
+    // Use the 5-second average (cp5s) instead of single-second CPS to avoid showing 0
+    // The cp5s value is the sum of counts over the last 5 seconds
+    // We'll use the 5-second uSv/h value that's already calculated in the sensor
+    float cps_usvh = gm_sensor->get_data().uSvh_5sec; // Already calculated 5-second average in uSv/h
+    
+    if (settings->get_cpm_usvh()) {
+      // Display CP5S (5-second average) big, uSv/h small for Survey Mode
+      M5.Lcd.setTextColor(gm_sensor->get_data().valid ? LCD_COLOR_DEFAULT : LCD_COLOR_STALE_INCOMPLETE, LCD_COLOR_BACKGROUND);
+      uint16_t cps_width = printIntFont(gm_sensor->get_data().cp5s, 0, 100, &fonts::Font7);
+      uint16_t ush_width = printFloatFont(cps_usvh, 4, 0, 140, &fonts::Font4);
 
-    if (gm_sensor->is_fresh() || force) {
-      // Display values
-      const auto& settings = workers.worker<LocalStorage>(k_worker_local_storage);
-      if (settings->get_cpm_usvh()) {
-        // Display CPM big, usvh small
-        M5.Lcd.setTextColor(gm_sensor->get_data().valid ? LCD_COLOR_DEFAULT : LCD_COLOR_STALE_INCOMPLETE, LCD_COLOR_BACKGROUND);
-        uint16_t cpm_width = printIntFont(gm_sensor->get_data().cpm_comp, 0, 100, &fonts::Font7);
-        uint16_t ush_width = printFloatFont(gm_sensor->get_data().uSvh, 4, 0, 140, &fonts::Font4);
-
-        // Display unit text with cleanup (CPM uSv/h)
-        M5.Lcd.setTextColor(LCD_COLOR_DEFAULT, LCD_COLOR_BACKGROUND);
-        M5.Lcd.fillRect(cpm_width, 52, 320 - cpm_width, 27, LCD_COLOR_BACKGROUND); // Prints blanks after cpm value, above CPM text
-        cpm_width += M5.Lcd.drawString(" CPM", cpm_width, 105, &fonts::Font4); // Prints after cpm value
-        M5.Lcd.fillRect(cpm_width, 74, 320 - cpm_width, 26, LCD_COLOR_BACKGROUND); // Prints blanks after CPM text
-        M5.Lcd.drawString(" uSv/h   ", 0 + ush_width, 140, &fonts::Font4); // Prints after ush value
-      } else {
-        M5.Lcd.setTextColor(gm_sensor->get_data().valid ? LCD_COLOR_DEFAULT : LCD_COLOR_STALE_INCOMPLETE, LCD_COLOR_BACKGROUND);
-        uint16_t ush_width = printFloatFont(gm_sensor->get_data().uSvh, 3, 0, 100, &fonts::Font7);
-        uint16_t cpm_width = printIntFont(gm_sensor->get_data().cpm_comp, 0, 140, &fonts::Font4);
-
-        // Display unit text with cleanup (CPM uSv/h)
-        M5.Lcd.setTextColor(LCD_COLOR_DEFAULT, LCD_COLOR_BACKGROUND);
-        M5.Lcd.fillRect(ush_width, 52, 320 - ush_width, 27, LCD_COLOR_BACKGROUND); // Prints blanks after cpm value, above CPM text
-        ush_width += M5.Lcd.drawString(" uSv/h", ush_width, 105, &fonts::Font4); // Prints after cpm value
-        M5.Lcd.fillRect(ush_width, 74, 320 - ush_width, 26, LCD_COLOR_BACKGROUND); // Prints blanks after CPM text
-        M5.Lcd.drawString(" CPM   ", 0 + cpm_width, 140, &fonts::Font4); // Prints after ush value
-      }
-    }
-
-    // Display GPS data always, change colour if not fresh
-    if (gps->is_fresh() || force) {
-      // Print survey data
+      // Display unit text with cleanup (CP5S uSv/h)
       M5.Lcd.setTextColor(LCD_COLOR_DEFAULT, LCD_COLOR_BACKGROUND);
-      const uint16_t lat_width = M5.Lcd.drawString("Latitude  :", 0, 157, &fonts::Font0);
-      const uint16_t lon_width = M5.Lcd.drawString("Longitude :", 0, 165, &fonts::Font0);
-      const uint16_t sat_width = M5.Lcd.drawString("Satellites:", 0, 173, &fonts::Font0);
+      M5.Lcd.fillRect(cps_width, 52, 320 - cps_width, 27, LCD_COLOR_BACKGROUND); // Prints blanks after cps value
+      cps_width += M5.Lcd.drawString(" CP5S", cps_width, 105, &fonts::Font4); // Prints after cps value
+      M5.Lcd.fillRect(cps_width, 74, 320 - cps_width, 26, LCD_COLOR_BACKGROUND); // Prints blanks after CP5S text
+      M5.Lcd.drawString(" uSv/h   ", 0 + ush_width, 140, &fonts::Font4); // Prints after ush value
+    } else {
+      // Display uSv/h big, CP5S small
+      M5.Lcd.setTextColor(gm_sensor->get_data().valid ? LCD_COLOR_DEFAULT : LCD_COLOR_STALE_INCOMPLETE, LCD_COLOR_BACKGROUND);
+      uint16_t ush_width = printFloatFont(cps_usvh, 3, 0, 100, &fonts::Font7);
+      uint16_t cps_width = printIntFont(gm_sensor->get_data().cp5s, 0, 140, &fonts::Font4);
 
-      // Print GPS values
-      M5.Lcd.setTextColor(gps->get_data().location_valid ? LCD_COLOR_DEFAULT : LCD_COLOR_STALE_INCOMPLETE, LCD_COLOR_BACKGROUND);
-      M5.Lcd.drawFloat(gps->get_data().latitude, 6, lat_width + 5, 157, &fonts::Font0);
-      M5.Lcd.drawFloat(gps->get_data().longitude, 6, lon_width + 5, 165, &fonts::Font0);
-      M5.Lcd.drawNumber(gps->get_data().numSV, sat_width + 5, 173, &fonts::Font0);
+      // Display unit text with cleanup (uSv/h CP5S)
+      M5.Lcd.setTextColor(LCD_COLOR_DEFAULT, LCD_COLOR_BACKGROUND);
+      M5.Lcd.fillRect(ush_width, 52, 320 - ush_width, 27, LCD_COLOR_BACKGROUND); // Prints blanks after uSv/h value
+      ush_width += M5.Lcd.drawString(" uSv/h", ush_width, 105, &fonts::Font4); // Prints after uSv/h value
+      M5.Lcd.fillRect(ush_width, 74, 320 - ush_width, 26, LCD_COLOR_BACKGROUND); // Prints blanks after uSv/h text
+      M5.Lcd.drawString(" CP5S   ", 0 + cps_width, 140, &fonts::Font4); // Prints after cp5s value
     }
+  }
+
+  // Always update GPS data when fresh
+  if (gps->is_fresh() || force) {
+    // Print survey data
+    M5.Lcd.setTextColor(LCD_COLOR_DEFAULT, LCD_COLOR_BACKGROUND);
+    const uint16_t lat_width = M5.Lcd.drawString("Latitude  :", 0, 157, &fonts::Font0);
+    const uint16_t lon_width = M5.Lcd.drawString("Longitude :", 0, 165, &fonts::Font0);
+    const uint16_t sat_width = M5.Lcd.drawString("Satellites:", 0, 173, &fonts::Font0);
+
+    // Print GPS values
+    M5.Lcd.setTextColor(gps->get_data().location_valid ? LCD_COLOR_DEFAULT : LCD_COLOR_STALE_INCOMPLETE, LCD_COLOR_BACKGROUND);
+    M5.Lcd.drawFloat(gps->get_data().latitude, 6, lat_width + 5, 157, &fonts::Font0);
+    M5.Lcd.drawFloat(gps->get_data().longitude, 6, lon_width + 5, 165, &fonts::Font0);
+    
+    // Make sure satellites are displayed correctly
+    uint8_t satellites = gps->get_data().numSV;
+    if (satellites == 0 && gps->is_fresh()) {
+      // If numSV is 0 but GPS is fresh, try to get satellites from satsInView
+      satellites = gps->get_data().satsInView;
+    }
+    M5.Lcd.drawNumber(satellites, sat_width + 5, 173, &fonts::Font0);
   }
 }
 
