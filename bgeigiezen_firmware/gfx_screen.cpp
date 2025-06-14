4a256245174f4656f5f4aff35fddf1b671e1812c
robouden <rob@safecast.org>
2025-06-14 23:52:40 +0900

    Some more small tweaks on status bar.

--- bgeigiezen_firmware/gfx_screen.cpp
+++ bgeigiezen_firmware/gfx_screen.cpp
@@ -346,7 +346,7 @@ void GFXScreen::handle_report(const worker_map_t& workers, const handler_map_t& 
           const auto& battery = workers.worker<BatteryIndicator>(k_worker_battery_indicator)->get_data();
           M5.Lcd.setTextColor(battery.isCharging ? LCD_COLOR_ACTIVITY : LCD_COLOR_DEFAULT, LCD_COLOR_BACKGROUND);
           M5.Lcd.printf("%d%%", battery.percentage);
-          pos += 30; 
+          pos += 28; 
 
           // Status icon: Geiger Tube
           M5.Lcd.setCursor(pos, 235);
@@ -357,7 +357,7 @@ void GFXScreen::handle_report(const worker_map_t& workers, const handler_map_t& 
             M5.Lcd.setTextColor(LCD_COLOR_DEFAULT, LCD_COLOR_BACKGROUND);
           }
           M5.Lcd.print("GM");
-          pos += 15; 
+          pos += 16; 
 
           // Status icon: GPS
           M5.Lcd.setCursor(pos, 235);
@@ -365,11 +365,11 @@ void GFXScreen::handle_report(const worker_map_t& workers, const handler_map_t& 
           if (!gps->active()) {
             M5.Lcd.setTextColor(_screen->has_required_gps() ? LCD_COLOR_ERROR : LCD_COLOR_INACTIVE, TFT_BLACK);
             M5.Lcd.print("GPS");
-            pos += 26; // Reduced from 24
+            pos += 30; 
           } else {
             M5.Lcd.setTextColor(gps->get_data().location_valid ? LCD_COLOR_ACTIVITY : LCD_COLOR_STALE_INCOMPLETE, TFT_BLACK);
             M5.Lcd.printf("GPS%d", gps->get_data().satsInView);
-            pos += 30; // Reduced from 30
+            pos += 34; 
           }
 
           // Status icon: SD
