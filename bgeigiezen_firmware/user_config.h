0fdd1c87f94b0bf21c0017f6aa07f94777d8bf28
robouden <rob@safecast.org>
2025-06-15 23:29:53 +0900

    Changed QR code for grafana for the last 24 hours.

--- bgeigiezen_firmware/user_config.h
+++ bgeigiezen_firmware/user_config.h
@@ -83,7 +83,7 @@ constexpr uint16_t API_SEND_SECONDS_DELAY = 300; // Posts every 5 minute by defa
 constexpr uint16_t API_SEND_SECONDS_DELAY_ALERT = 5; // Posts every 5 seconds when alerted
 constexpr uint16_t API_SEND_SECONDS_DELAY_ROAMING = 5; // Posts every 5 seconds when roaming
 
-constexpr char FIXED_MODE_GRAFANA_URL[] = "https://tinyurl.com/34yr2tzv?var-device_urn_name=geigiecast-zen:%d";
+constexpr char FIXED_MODE_GRAFANA_URL[] = "https://tinyurl.com/34yr2tzv?var-device_urn_name=geigiecast-zen:%d&from=now-24h&to=now";
 
 // Other
 constexpr char SCREENSAVER_TEXT[] = VERSION_STRING;
